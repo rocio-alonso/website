1ff128c73ca5673d66c4b7d7f06cda9c4b5136f8
JulianLipa <lipajulianp@gmail.com>
2024-12-12 15:19:23 -0300

    animations added and uploads v9

--- src/app/obras/page.js
+++ src/app/obras/page.js
@@ -1,8 +1,6 @@
 import React from "react";
 import "@/app/globals.css";
-import Exposiciones from "../components/Exposiciones/Exposiciones";
 import styles from "@/app/obras/page.module.css";
-import Obras from "@/app/components/Obras/Obras"
 
 const page = async () => {
   return (
@@ -12,7 +10,6 @@ const page = async () => {
           <h2 className="text-6xl thin">Mis obras</h2>
         </div>
       </div>
-      <Obras />
     </main>
   );
 };
